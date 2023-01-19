baf493332599e72c62487168c73e1e7c009effcd
fyl <97fyl97@gmail.com>
2023-01-19 18:42:30 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
 
-// const modalBtns = document.querySelectorAll('.modal');
-const modalOverlay = document.querySelectorAll('.modal-wrapper');
-const modals = document.querySelectorAll('.modal');
-const btnСloseModal = document.querySelectorAll('.btn--close-modal');
+  const modalBtns = document.querySelectorAll('.works-card');
+  const modalOverlay = document.querySelectorAll('.modal-wrapper');
+  const modals = document.querySelectorAll('.modal');
+  const btnСloseModal = document.querySelectorAll('.btn--close-modal');
 
-var swiper = new Swiper(".swiper-container_1", {
+  var swiper = new Swiper(".swiper-container_1", {
     spaceBetween: 30,
     slidesPerView: 3,
     centeredSlides: true,
@@ -16,7 +16,7 @@ var swiper = new Swiper(".swiper-container_1", {
       499: {
         slidesPerView: 6,
       },
-  
+
       999: {
         slidesPerView: 8,
       },
@@ -40,7 +40,7 @@ var swiper = new Swiper(".swiper-container_1", {
       499: {
         slidesPerView: 6,
       },
-  
+
       999: {
         slidesPerView: 8,
       },
@@ -56,45 +56,66 @@ var swiper = new Swiper(".swiper-container_1", {
   });
   console.log(modals, "modals");
 
-  modals.forEach((el) => {
+  modalBtns.forEach((el) => {
     console.log(el);
-   
-	el.addEventListener('click', (e) => {
-    console.log(e.target, "e.target");
- 
-		let path = e.currentTarget.getAttribute('data-path');
-    el.classList.add('active')
-		modals.forEach((el) => {
-			el.classList.remove('modal--open');
-		});
-    console.log(`[data-target="${path}"]`,  "[data-target=path]");
-		document.querySelector(`[data-target="${path}"]`).classList.add('modal--open');
-	
-	});
-});
-modals.forEach((el) => {
-  el.addEventListener('click', (e) => {
-  modals.forEach((el) => {
-    el.classList.remove('modal--open');
-    el.classList.remove('active');
-  });
-});
-});
-document.addEventListener('click',e => console.log(e.target))
-// document.addEventListener('click', (e) => {
-// let ddd =  e.closest('.modal')
-// console.log(ddd, "ddd");
-// });
-// modalOverlay.forEach((el) => {
-//   el.addEventListener('click', (e) => {
-// 	console.log(e.target);
 
-// 	if (e.target == modalOverlay) {
-// 		modalOverlay.classList.remove('modal-overlay--visible');
-// 		modals.forEach((el) => {
-// 			el.classList.remove('modal--visible');
-// 		});
-// 	}
-// });
-// });
+    el.addEventListener('click', (e) => {
+      console.log(e.target, "e.target", e.currentTarget.getAttribute('data-path'));
+
+      let path = e.currentTarget.getAttribute('data-path');
+      el.classList.add('active')
+      modals.forEach((el) => {
+        el.classList.remove('modal--open');
+      });
+      console.log(`[data-target="${path}"]`, "[data-target=path]");
+      document.querySelector(`[data-target="${path}"]`).classList.add('modal--open');
+
+    });
+  });
+
+  btnСloseModal.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      console.log(el, "el btnСloseModal")
+      modals.forEach((el) => {
+        el.classList.remove('modal--open');
+        el.classList.remove('active');
+      });
+      el.classList.remove('modal--open');
+      el.classList.remove('active');
+
+
+    });
+  });
+
+  modals.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      if (e.currentTarget.classList.contains !== "modal-wrapper") {
+        modals.forEach((el) => {
+          console.log(el, "el e.currentTarget.classList.contains")
+          el.classList.remove('modal--open');
+          el.classList.remove('active');
+        });
+        el.classList.remove('modal--open');
+        el.classList.remove('active');
+      }
+
+    });
+  });
+  document.addEventListener('click', e => console.log(e.target))
+  // document.addEventListener('click', (e) => {
+  // let ddd =  e.closest('.modal')
+  // console.log(ddd, "ddd");
+  // });
+  // modalOverlay.forEach((el) => {
+  //   el.addEventListener('click', (e) => {
+  // 	console.log(e.target);
+
+  // 	if (e.target == modalOverlay) {
+  // 		modalOverlay.classList.remove('modal-overlay--visible');
+  // 		modals.forEach((el) => {
+  // 			el.classList.remove('modal--visible');
+  // 		});
+  // 	}
+  // });
+  // });
 }, false);
