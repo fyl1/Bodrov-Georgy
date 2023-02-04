4e0f06445bb13bde7126015f9eb211a6138a3144
fyl <97fyl97@gmail.com>
2023-02-04 22:10:52 +0200

    fix header,  card, line

--- js/script.js
+++ js/script.js
@@ -8,54 +8,54 @@ document.addEventListener('DOMContentLoaded', function () {
   const body = document.querySelector('.body');
   const anchors = document.querySelectorAll('a[href*="#"]')
 
-  var swiper = new Swiper(".swiper-container_1", {
-    spaceBetween: 30,
-    slidesPerView: 3,
-    centeredSlides: true,
-    loop: true,
-    breakpoints: {
-      // when window width is <= 499px
-      499: {
-        slidesPerView: 6,
-      },
+  // var swiper = new Swiper(".swiper-container_1", {
+  //   spaceBetween: 30,
+  //   slidesPerView: 3,
+  //   centeredSlides: true,
+  //   loop: true,
+  //   breakpoints: {
+  //     // when window width is <= 499px
+  //     499: {
+  //       slidesPerView: 6,
+  //     },
   
-      999: {
-        slidesPerView: 8,
-      },
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-  var swiper1 = new Swiper(".swiper-container_2", {
-    spaceBetween: 30,
-    slidesPerView: 3,
-    centeredSlides: true,
-    loop: true,
-    breakpoints: {
-      // when window width is <= 499px
-      499: {
-        slidesPerView: 6,
-      },
+  //     999: {
+  //       slidesPerView: 8,
+  //     },
+  //   },
+  //   autoplay: {
+  //     delay: 2000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
+  // var swiper1 = new Swiper(".swiper-container_2", {
+  //   spaceBetween: 30,
+  //   slidesPerView: 3,
+  //   centeredSlides: true,
+  //   loop: true,
+  //   breakpoints: {
+  //     // when window width is <= 499px
+  //     499: {
+  //       slidesPerView: 6,
+  //     },
   
-      999: {
-        slidesPerView: 8,
-      },
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  //     999: {
+  //       slidesPerView: 8,
+  //     },
+  //   },
+  //   autoplay: {
+  //     delay: 2000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
 
   modalBtns.forEach((el) => {
 
